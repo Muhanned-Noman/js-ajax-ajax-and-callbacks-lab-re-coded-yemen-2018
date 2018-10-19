6b1b9c46bfdb48d0c362ca01a76b96a7238f7c1e
Muhanned Abdulhaq Mohammed Noman <muhanned.noman@gmail.com>
2018-10-19 12:06:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,24 @@ $(document).ready(function (){
       </div>
       <hr>
     `
-}
+  }
+  
+  let showCommit = (commit) => {
+    return `<li><h3>${commit.sha}</h3><p>${commit.commit.message}</p></li>`
+  }
+
+  let renderCommits = (data) => {
+    let result = data.map((commit)=>renderCommit(commit)).join('')
+    return `<ul>${result}</ul>`
+  }
+
+  let showCommits = (el) => {
+    $.get(`https://api.github.com/repos/${el.dataset.owner}/${el.dataset.repository}/commits`, data => {
+      $('#details').html(renderCommits(data))
+    }).fail(error => {
+      displayError()
+    })
+  }
   
   let showError = () => $('#errors').html("there's been an error. Please try again.")
 });
