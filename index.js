d0634d85b14cae7838e79fe1d298fb1c66e2b1f9
Muhanned Abdulhaq Mohammed Noman <muhanned.noman@gmail.com>
2018-10-19 12:05:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,5 +10,16 @@ $(document).ready(function (){
   
   let showSearchResults = (data) => data.items.map( result => showSearchResult(result))
   
+  let showSearchResult = (result) => {
+  return `
+      <div>
+        <h2><a href="${result.html_url}">${result.name}</a></h2>
+        <p><a href="#" data-repository="${result.name}" data-owner="${result.owner.login}" onclick="showCommits(this)">Show Commits</a></p>
+        <p>${result.description}</p>
+      </div>
+      <hr>
+    `
+}
+  
   let showError = () => $('#errors').html("there's been an error. Please try again.")
 });
