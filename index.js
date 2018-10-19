79de816ad0828a4ed0b4ddaa40ecba95b21289df
Muhanned Abdulhaq Mohammed Noman <muhanned.noman@gmail.com>
2018-10-19 11:58:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 $(document).ready(function (){
+  var searchRepositories = () => {
+  const searchTerms = $('#searchTerms').val()
+  $.get(`https://api.github.com/search/repositories?q=${searchTerms}`, data => {
+      $('#results').html(renderSearchResults(data))
+    }).fail(error => {
+      displayError()
+    })
+}
 });
